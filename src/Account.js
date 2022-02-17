50a80f8b5ec97201606bc0537a380f8505f6a5ea
Linda Zhou <lhmzhou@gmail.com>
2022-02-17 00:02:50 -0600

    show timeline

--- src/Account.js
+++ src/Account.js
@@ -2,18 +2,26 @@ module.exports = class Account {
 
     constructor(user){
         this.user = user; 
-    }
+    };
 
     user = "";
     timeline = [];
     following = [];
     followers = [];
-    wall =[];
+
+    follow(user) {
+        this.following.push(user);
+        user.followers.push(this);
+    };
 
     publish(content) {
         const timestamp = Date.now()
         let post = [content, timestamp]
         this.timeline.push(post);
         this.wall.push(post);
-    }
+    };
+
+    showTimeline() {
+        return this.timeline.reverse();
+    };
 }
